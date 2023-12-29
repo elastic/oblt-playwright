b0f2162944918b49aea547e7d4e487033cc97c86
Andrei_Bialenik <andrei.belenik@gmail.com>
2023-12-29 19:42:19 +0100

    Add a test step adding integration to API test suite.

--- tests/api/api_tests.spec.ts
+++ tests/api/api_tests.spec.ts
@@ -1,23 +1,48 @@
 import {test, expect} from '@playwright/test';
 
-test.describe('Fleet', () => {
-    test('Create a new policy in Fleet', async({request}) => {  
-        var counter = 0;
-        do {
-            var policyName = "Playwright Test Policy " + counter++;
-            console.log(policyName + " already exists.");
-            var response = await request.post('/api/fleet/agent_policies', {
+test.describe.serial('Fleet', () => {
+    test('Policy actions', async({request}) => {  
+        const policy = await test.step('Create a new policy', async () => { 
+            var counter = 1;
+            do {
+                var policyName = "Playwright Test Policy " + counter++;
+                console.log(policyName + " exists.");
+                var response = await request.post('/api/fleet/agent_policies', {
+                    data: {
+                        "name":policyName,
+                        "namespace":"default",
+                        "monitoring_enabled":["logs","metrics"],
+                    }
+                })
+            } while (response.status() === 409)
+
+            expect(response.status()).toBe(200);
+            const name = await response.text();
+            expect(name).toContain(policyName);
+            const responseBody = JSON.parse(await response.text());
+            console.log("Policy has been created.");
+            return responseBody;
+    });
+
+        const integration = await test.step('Add nginx integration', async() => {  
+            var response = await request.post('/api/fleet/package_policies', {
                 data: {
-                    "name":policyName,
+                    "name":"nginx-playwright-test",
                     "namespace":"default",
-                    "monitoring_enabled":["logs","metrics"],
+                    "enabled":"true",
+                    "policy_id": policy.item.id,
+                    "package":{"name":"nginx", "title":"Nginx", "version":"1.17.0"},
+                    "inputs":[],
                 }
             })
-        } while (response.status() === 409)
 
-        expect(response.status()).toBe(200);
-        const name = await response.text();
-        expect(name).toContain(policyName);
-        console.log(await response.json());
-    });
+            expect(response.status()).toBe(200);
+            const name = await response.text();
+            expect(name).toContain("nginx-playwright-test");
+            const responseBody = JSON.parse(await response.text());
+            console.log("Integration has been added.");
+            return responseBody;
+        });
+
+});
 });
