bffef40c0e79dc24696a26116c2d7705668d1cdb
Andrei_Bialenik <andrei.belenik@gmail.com>
2024-03-04 18:45:42 +0100

    Add log messages to authentication scripts.

--- tests/serverless.auth.ts
+++ tests/serverless.auth.ts
@@ -3,13 +3,13 @@ import { STORAGE_STATE } from "../playwright.config";
 
 serverless_auth('Authentication', async ( {page} ) => {
   await page.goto(process.env.ELASTIC_URL);
-  await page.waitForLoadState('networkidle');
+  console.log(`...waiting for login page elements to appear.`);
   await page.getByTestId('login-username').click();
   await page.getByTestId('login-username').fill(process.env.ELASTIC_USERNAME);
   await page.getByTestId('login-password').click();
   await page.getByTestId('login-password').fill(process.env.ELASTIC_PASSWORD);
   await page.getByTestId('login-button').click();
-  await page.waitForLoadState('networkidle');
-  await expect(page).toHaveTitle('Onboarding - Elastic');
+  console.log(`...waiting for page to have 'Onboarding - Elastic' title.`);
+  await expect(page, 'User is authenticated').toHaveTitle('Onboarding - Elastic');
   await page.context().storageState({path: STORAGE_STATE});
 });
--- tests/stateful.auth.ts
+++ tests/stateful.auth.ts
@@ -3,11 +3,13 @@ import { STORAGE_STATE } from "../playwright.config";
 
 ess_setup('Authentication', async ({page}) => {
   await page.goto(process.env.ELASTIC_URL);
+  console.log(`...waiting for login page elements to appear.`);
   await page.getByRole('button', { name: 'Log in with Elasticsearch' }).click();
   await page.getByLabel('Username').fill(process.env.ELASTIC_USERNAME);
   await page.getByLabel('Password', { exact: true }).click();
-  await page.getByLabel('Password', { exact: true }).fill(process.env.ELASTIC_PASSWORD, { timeout: 20000});
+  await page.getByLabel('Password', { exact: true }).fill(process.env.ELASTIC_PASSWORD);
   await page.getByRole('button', { name: 'Log in' }).click();
-  await expect(page).toHaveTitle('Home - Elastic', { timeout: 50000});
+  console.log(`...waiting for page to have 'Home - Elastic' title.`);
+  await expect(page, 'User is authenticated').toHaveTitle('Home - Elastic');
   await page.context().storageState({path: STORAGE_STATE});
 });
