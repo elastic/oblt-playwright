d9f6bbd955b1e17fc40741b480b25de95cad0e19
Andrei_Bialenik <andrei.belenik@gmail.com>
2024-03-07 11:48:36 +0100

    Add error handling to authentication scripts.

--- tests/serverless.auth.ts
+++ tests/serverless.auth.ts
@@ -1,7 +1,8 @@
-import { expect, test as serverless_auth } from "@playwright/test";
+import { test as serverless_auth } from "@playwright/test";
 import { STORAGE_STATE } from "../playwright.config";
+import { waitForOneOf } from "../src/types.ts";
 
-serverless_auth('Authentication', async ( {page} ) => {
+serverless_auth('Authentication', async ({page}) => {
   await page.goto(process.env.ELASTIC_URL);
   console.log(`...waiting for login page elements to appear.`);
   await page.getByTestId('login-username').click();
@@ -9,7 +10,16 @@ serverless_auth('Authentication', async ( {page} ) => {
   await page.getByTestId('login-password').click();
   await page.getByTestId('login-password').fill(process.env.ELASTIC_PASSWORD);
   await page.getByTestId('login-button').click();
-  console.log(`...waiting for page to have 'Onboarding - Elastic' title.`);
-  await expect(page, 'User is authenticated').toHaveTitle('Onboarding - Elastic');
-  await page.context().storageState({path: STORAGE_STATE});
+  
+  const [ index ] = await waitForOneOf([
+    page.locator('xpath=//div[@data-test-subj="svlObservabilitySideNav"]'),
+    page.locator('xpath=//div[@data-test-id="login-error"]'),
+  ]);
+  const isAuthenticated = index === 0;
+  if (isAuthenticated) {
+    await page.context().storageState({path: STORAGE_STATE});
+  } else {
+    console.log('Username or password is incorrect.');
+    throw new Error('Authentication is failed.');
+  }
 });
--- tests/stateful.auth.ts
+++ tests/stateful.auth.ts
@@ -1,7 +1,8 @@
-import { expect, test as ess_setup } from "@playwright/test";
+import { test as ess_auth } from "@playwright/test";
 import { STORAGE_STATE } from "../playwright.config";
+import { waitForOneOf } from "../src/types.ts";
 
-ess_setup('Authentication', async ({page}) => {
+ess_auth('Authentication', async ({page}) => {
   await page.goto(process.env.ELASTIC_URL);
   console.log(`...waiting for login page elements to appear.`);
   await page.getByRole('button', { name: 'Log in with Elasticsearch' }).click();
@@ -9,7 +10,16 @@ ess_setup('Authentication', async ({page}) => {
   await page.getByLabel('Password', { exact: true }).click();
   await page.getByLabel('Password', { exact: true }).fill(process.env.ELASTIC_PASSWORD);
   await page.getByRole('button', { name: 'Log in' }).click();
-  console.log(`...waiting for page to have 'Home - Elastic' title.`);
-  await expect(page, 'User is authenticated').toHaveTitle('Home - Elastic');
-  await page.context().storageState({path: STORAGE_STATE});
+
+  const [ index ] = await waitForOneOf([
+    page.locator('xpath=//a[@aria-label="Elastic home"]'),
+    page.locator('xpath=//div[@data-test-subj="loginErrorMessage"]'),
+  ]);
+  const isAuthenticated = index === 0;
+  if (isAuthenticated) {
+    await page.context().storageState({path: STORAGE_STATE});
+  } else {
+    console.log('Username or password is incorrect.');
+    throw new Error('Authentication is failed.');
+  }
 });
