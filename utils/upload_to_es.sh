3795a7f418739f521df35ac02a4940f7db9c901c
Andrei_Bialenik <andrei.belenik@gmail.com>
2024-04-26 12:26:42 +0200

    Add a job to upload test reports to ES.

--- utils/upload_to_es.sh
+++ utils/upload_to_es.sh
@@ -1,5 +1,3 @@
-source .env
-
-for file in ./test-results/*.json; do
-    curl -XPOST "$ELASTIC_ES/$ELASTIC_INDEX/_doc/?pretty" -H "Authorization: $API_KEY" -H "Content-Type: application/json" --data-binary "@$file"
+for file in ./perf-test-report/*.json; do
+    curl -XPOST "$ELASTIC_ES/$ELASTIC_SLO_INDEX/_doc/?pretty" -H "Authorization: $API_KEY" -H "Content-Type: application/json" --data-binary "@$file"
 done
