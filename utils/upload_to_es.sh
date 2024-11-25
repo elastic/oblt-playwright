9e5a405599bddd0987a47b894670b11c7848d6c3
Andrei Bialenik <34958359+ablnk@users.noreply.github.com>
2024-11-25 16:25:49 +0100

    Update upload_to_es.sh

--- utils/upload_to_es.sh
+++ utils/upload_to_es.sh
@@ -1,4 +1,4 @@
 for file in /home/runner/work/oblt-playwright/perf_test_*.json; do
     echo "Found file: $file"
-    curl -XPOST "$ELASTICSEARCH_HOST/$REPORTING_CLUSTER_INDEX/_doc/?pretty" -H "Authorization: $API_KEY" -H "Content-Type: application/json" --data-binary "@$file"
+    curl -XPOST "$ELASTICSEARCH_HOST/$REPORTING_CLUSTER_INDEX/_doc/?pretty" -H "Authorization: $REPORTING_CLUSTER_API_KEY" -H "Content-Type: application/json" --data-binary "@$file"
 done
