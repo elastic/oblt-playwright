8816c2e7151b91496a98bae5e9eee9721f5a2c32
Andrei Bialenik <34958359+ablnk@users.noreply.github.com>
2024-11-25 17:55:06 +0100

    Update upload_to_es.sh

--- utils/upload_to_es.sh
+++ utils/upload_to_es.sh
@@ -4,5 +4,5 @@ REPORT_DIR=/home/runner/work/oblt-playwright/
 FILES=$(find "$REPORT_DIR" -type f -name "*.json" ! -name "results.json")
 for file in $FILES; do
     echo "Found file: $file"
-    curl -XPOST "$REPORTING_CLUSTER_ES/$REPORTING_CLUSTER_INDEX/_doc/?pretty" -H "Authorization: $REPORTING_CLUSTER_API_KEY" -H "Content-Type: application/json" --data-binary "@$file"
+    curl -XPOST "$REPORTING_CLUSTER_ES$REPORTING_CLUSTER_INDEX/_doc/?pretty" -H "Authorization: $REPORTING_CLUSTER_API_KEY" -H "Content-Type: application/json" --data-binary "@$file"
 done
