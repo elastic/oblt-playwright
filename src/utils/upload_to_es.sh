961ba1620fd22b7404e49a21712083f8b0d2c66d
Alejandro Colomina <alejandro.colomina@gmail.com>
2025-10-21 16:11:54 +0200

    chore: env vars fallbacks (#373)

--- src/utils/upload_to_es.sh
+++ src/utils/upload_to_es.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPORT_DIR=/home/runner/work/oblt-playwright/
+REPORT_DIR="${REPORT_DIR:-/home/runner/work/oblt-playwright/}"
 REPORT_CLUSTER_ES=${REPORT_CLUSTER_ES%/}
 FILES=$(find "$REPORT_DIR" -type f -name "20*.json" ! -name "results.json" ! -name "*authentication.json" ! -name "package.json" ! -name "package-lock.json")
 for file in $FILES; do
