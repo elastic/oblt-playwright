33053d5843ba92768293c6636ff649d1af54c176
Andrei Bialenik <andrei.bialenik@elastic.co>
2025-10-21 14:33:30 +0200

    feat: wrap authentication logic into step

--- tests/auth.ts
+++ tests/auth.ts
@@ -1,11 +1,20 @@
 import { test, expect } from "@playwright/test";
 import { STORAGE_STATE } from "../playwright.config";
-import { waitForOneOf, fetchClusterData } from "../src/helpers.ts";
+import { fetchClusterData, testStep, waitForOneOf, writeJsonReport } from "../src/helpers.ts";
 import { KIBANA_HOST, KIBANA_USERNAME, KIBANA_PASSWORD } from "../src/env.ts";
 import { logger } from "../src/logger.ts";
 
-test("Authentication", async ({ page }) => {
-  const clusterData: any = await fetchClusterData();
+let clusterData: any;
+const testStartTime: string = new Date().toISOString();
+
+test.afterAll('Print test results', async ({ }, testInfo) => {
+  const stepData = (testInfo as any).stepData;
+  await writeJsonReport(clusterData, testInfo, testStartTime, undefined, stepData);
+});
+
+test("Authentication", async ({ page }, testInfo) => {
+  let stepData: object[] = [];
+  clusterData = await fetchClusterData();
   const buildFlavor: string | undefined = clusterData?.version?.build_flavor;
 
   if (!buildFlavor) {
@@ -13,24 +22,31 @@ test("Authentication", async ({ page }) => {
   }
 
   logger.info(`Detected build flavor: ${buildFlavor}`);
-  logger.info("Navigating to Kibana host");
-  await page.goto(KIBANA_HOST);
+
+  let index: number;
 
   if (buildFlavor === "serverless") {
-    // Serverless workflow
-    logger.info("Waiting for login page elements to appear, filling credentials (Serverless)");
-    await page.locator('[data-test-id="login-username"]').click();
-    await page.locator('[data-test-id="login-username"]').fill(KIBANA_USERNAME);
-    await page.locator('[data-test-id="login-password"]').click();
-    await page.locator('[data-test-id="login-password"]').fill(KIBANA_PASSWORD);
-    await page.locator('[data-test-id="login-button"]').click();
+    index = await testStep('step01', stepData, page, async () => {
+      logger.info("Navigating to Kibana host");
+      await page.goto(KIBANA_HOST);
+      
+      // Serverless workflow
+      logger.info("Waiting for login page elements to appear, filling credentials (Serverless)");
+      await page.locator('[data-test-id="login-username"]').click();
+      await page.locator('[data-test-id="login-username"]').fill(KIBANA_USERNAME);
+      await page.locator('[data-test-id="login-password"]').click();
+      await page.locator('[data-test-id="login-password"]').fill(KIBANA_PASSWORD);
+      await page.locator('[data-test-id="login-button"]').click();
 
-    logger.info("Logging in to Kibana (Serverless)");
-    const [index] = await waitForOneOf([
-      page.locator('xpath=//nav[@data-test-subj="projectLayoutSideNav"]'),
-      page.locator('xpath=//h1[contains(text(),"Select your space")]'),
-      page.locator('xpath=//div[@data-test-id="login-error"]'),
-    ]);
+      logger.info("Logging in to Kibana (Serverless)");
+      const [result] = await waitForOneOf([
+        page.locator('xpath=//nav[@data-test-subj="projectLayoutSideNav"]'),
+        page.locator('xpath=//h1[contains(text(),"Select your space")]'),
+        page.locator('xpath=//div[@data-test-id="login-error"]'),
+      ]);
+      
+      return result;
+    }, 'Navigating to Kibana host and logging in (Serverless)');
 
     const spaceSelector = index === 1;
     const isAuthenticated = index === 0;
@@ -49,20 +65,27 @@ test("Authentication", async ({ page }) => {
       throw new Error("Authentication is failed");
     }
   } else if (buildFlavor === "default") {
-    // ECH workflow
-    logger.info("Waiting for login page elements to appear, filling credentials (ECH)");
-    await page.getByRole("button", { name: "Log in with Elasticsearch" }).click();
-    await page.getByLabel("Username").fill(KIBANA_USERNAME);
-    await page.getByLabel("Password", { exact: true }).click();
-    await page.getByLabel("Password", { exact: true }).fill(KIBANA_PASSWORD);
-    await page.getByRole("button", { name: "Log in" }).click();
+    index = await testStep('step01', stepData, page, async () => {
+      logger.info("Navigating to Kibana host");
+      await page.goto(KIBANA_HOST);
+      
+      // ECH workflow
+      logger.info("Waiting for login page elements to appear, filling credentials (ECH)");
+      await page.getByRole("button", { name: "Log in with Elasticsearch" }).click();
+      await page.getByLabel("Username").fill(KIBANA_USERNAME);
+      await page.getByLabel("Password", { exact: true }).click();
+      await page.getByLabel("Password", { exact: true }).fill(KIBANA_PASSWORD);
+      await page.getByRole("button", { name: "Log in" }).click();
 
-    logger.info("Logging in to Kibana (ECH)");
-    const [index] = await waitForOneOf([
-      page.locator('xpath=//div[@data-test-subj="helpMenuButton"]'),
-      page.locator('xpath=//h1[contains(text(),"Select your space")]'),
-      page.locator('xpath=//div[@data-test-subj="loginErrorMessage"]'),
-    ]);
+      logger.info("Logging in to Kibana (ECH)");
+      const [result] = await waitForOneOf([
+        page.locator('xpath=//div[@data-test-subj="helpMenuButton"]'),
+        page.locator('xpath=//h1[contains(text(),"Select your space")]'),
+        page.locator('xpath=//div[@data-test-subj="loginErrorMessage"]'),
+      ]);
+      
+      return result;
+    }, 'Navigating to Kibana host and logging in (ECH)');
 
     const spaceSelector = index === 1;
     const isAuthenticated = index === 0;
@@ -83,4 +106,6 @@ test("Authentication", async ({ page }) => {
   } else {
     throw new Error(`Unknown build flavor: ${buildFlavor}`);
   }
+
+  (testInfo as any).stepData = stepData;
 });
