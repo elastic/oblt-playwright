61bee9bacbec58b1a8fd3e1e19fbb9bbcfa14dd6
Andrei Bialenik <andrei.bialenik@elastic.co>
2025-10-21 09:38:45 +0200

    chore: prepend ApiKey to the REPORT_CLUSTER_API_KEY in the Authorization header

--- src/utils/upload_to_es.sh
+++ src/utils/upload_to_es.sh
@@ -4,5 +4,5 @@ REPORT_DIR=/home/runner/work/oblt-playwright/
 FILES=$(find "$REPORT_DIR" -type f -name "20*.json" ! -name "results.json" ! -name "*authentication.json" ! -name "package.json" ! -name "package-lock.json")
 for file in $FILES; do
     echo "Found file: $file"
-    curl -XPOST "$REPORT_CLUSTER_ES/$REPORT_CLUSTER_INDEX/_doc/?pretty" -H "Authorization: $REPORT_CLUSTER_API_KEY" -H "Content-Type: application/json" --data-binary "@$file"
+    curl -XPOST "$REPORT_CLUSTER_ES/$REPORT_CLUSTER_INDEX/_doc/?pretty" -H "Authorization: ApiKey $REPORT_CLUSTER_API_KEY" -H "Content-Type: application/json" --data-binary "@$file"
 done
